2b53357a9a205e48dfb7c6c191379c612b7b4aee
marinrika <marinrika2016@gmail.com>
2023-01-27 23:29:34 +0300

    feat: test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let brackets = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let pair = bracketsConfig[i];
+    brackets[pair[0]] = pair[1];
+  }
+  if (Object.keys(brackets).includes(str[0])) stack.push(str[0]);
+  else return false;
+  for (let i = 1; i < str.length; i++) {
+    if (Object.values(brackets).includes(str[i])) {
+      if (str[i] === brackets[stack[stack.length - 1]]) stack.pop();
+      else if (Object.keys(brackets).includes(str[i])) stack.push(str[i]);
+      else return false;
+    } else stack.push(str[i]);
+  }
+  return stack.length === 0;
+};
